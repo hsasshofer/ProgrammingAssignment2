19e33fcdc39b5cfe208fc86122274e8d6024ca2a
hsasshofer <hs@ieee.org>
2014-04-09 12:30:52 +0200

    Built solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##  makeCacheMatix
+#
+#   Creates an object for a (square!) matrix to prepare for caching its inverse
+#   containing a setter and getter for the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- matrix()
+    set <- function(y) {
+        x <<- y
+        i <<- matrix()
+    }
+    get <- function() x
+    setinv <- function(inv) i <<- inv
+    getinv <- function() i
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+    
 }
 
-
-## Write a short comment describing this function
+##  cacheSolve
+#
+#   returns the inverse of a (square!) matrix's caching object
+#   if no inverse exists yet will store it will determine it an via its setter
+#   or return the cached value otherwise
+#
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    emptyMatrix <- function (i) {
+        #   an "empty" matrix has dimension 1 by 1 and its only value is NA
+        (identical(dim(i), as.integer(c(1, 1))) && is.na(i[1,1])) 
+    }
+    
+    i <- x$getinv()
+    if(!emptyMatrix(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinv(i)
+    i
 }
