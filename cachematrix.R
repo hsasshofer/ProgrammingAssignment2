7f983add0f9aff7b4112f1809e257efea8148436
hsasshofer <hs@ieee.org>
2014-04-11 12:31:20 +0200

    Improved comments and formatting

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ##  makeCacheMatrix
 #
-#   Creates an object for a (square!) matrix to prepare for caching its inverse
+#   Creates an "object" for a (square!) matrix to prepare for caching its inverse
 #   containing a getter and setter (returning the value set) 
 #   for the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-    i <- matrix()
+    i <- matrix()            # clear cache for inverse initially
     set <- function(y) {
-        i <<- matrix()
+        i <<- matrix()       # setter changes matrinx and clears cache
         x <<- y
     }
     get <- function() x
     setinv <- function(inv) i <<- inv
     getinv <- function() i
-    list(set = set, get = get,
-         setinv = setinv,
-         getinv = getinv)
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 ##  cacheSolve
@@ -30,16 +25,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     emptyMatrix <- function (i) {
-        #   an "empty" matrix has dimension 1 by 1 and its only value is NA
+        # an "empty" matrix has dimension 1 by 1 and its only value is NA
         (identical(dim(i), as.integer(c(1, 1))) && is.na(i[1,1])) 
     }
-    
-    i <- x$getinv()
-    if(!emptyMatrix(i)) {
+
+    i <- x$getinv()                    # try to get cached value
+    if(!emptyMatrix(i)) {              # found one, return it
         message("getting cached data")
         return(i)
     }
-    data <- x$get()
-    i <- solve(data, ...)
-    x$setinv(i)
+    data <- x$get()                    # not found, get matrix contents
+    i <- solve(data, ...)              # invert it
+    x$setinv(i)                        # store / return the inverse
 }
